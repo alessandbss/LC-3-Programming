83b70b1e16df9aaea99e3dfa95c65343e0fb45ad
alessandbss <163196470+alessandbss@users.noreply.github.com>
2026-02-23 17:19:32 -0300

    Add documentation to assembly code

--- mult.asm
+++ mult.asm
@@ -1,5 +1,7 @@
-.ORIG x3000
+.ORIG x3000         ; Diz ao simulador para alocar o programa a partir do endereço x3000 
 
-LD R1, A
-LD R2, B
-AND R3, R3 #0
+LD R1, A            ; Carrega o valor de A (5) da memória para o Registrador 1 
+LD R2, B            ; Carrega o valor de B (4) para o Registrador 2 (ele será o contador) 
+AND R3, R3, #0      ; Zera o Registrador 3 usando operação lógica. R3 guardará o resultado 
+
+.END                ; Diretiva que indica ao montador (assembler) o fim do código 
